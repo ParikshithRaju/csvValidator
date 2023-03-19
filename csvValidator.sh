adc3dd792b3d8387bb56402a2e1288b795c74e9b
Parikshith C R <parikshith.cr@tsgforce.com>
2023-03-19 12:03:34 +0530

    added min and max length validation

--- csvValidator.sh
+++ csvValidator.sh
@@ -27,15 +27,21 @@ do
     columnSchema="$(jq ".${key}" $2)";
     isRequiredTrue="$(echo $columnSchema | jq '.required')";
     requiredLength="$(echo $columnSchema | jq '.length')";
+    minLength="$(echo $columnSchema | jq '.minLength')";
+    maxLength="$(echo $columnSchema | jq '.maxLength')";
     columnsInCSV="$(sed -n '1p' $1)";
     columnIndex="$(getColumnIndexByString $columnsInCSV $key)";
     columnIndexesOfColumnsToBeValidated[keyIndex]=$columnIndex;
     isRequiredValidationArray[keyIndex]=$isRequiredTrue
     lengthValidationArray[keyIndex]=$requiredLength
+    maxLengthValidationArray[keyIndex]=$maxLength
+    minLengthValidationArray[keyIndex]=$minLength
     (( ++keyIndex ))
 done
 columnIndexs=${columnIndexesOfColumnsToBeValidated[@]}
 _isRequiredValidationArray=${isRequiredValidationArray[@]}
 _lengthValidationArray=${lengthValidationArray[@]}
+_maxLengthValidationArray=${maxLengthValidationArray[@]}
+_minLengthValidationArray=${minLengthValidationArray[@]}
 
-awk -F , -v columnIndexs="$columnIndexs" -v _isRequiredValidationArray="$_isRequiredValidationArray" -v _lengthValidationArray="$_lengthValidationArray" -f schemaValidator.awk $1
+awk -F , -v columnIndexs="$columnIndexs" -v _isRequiredValidationArray="$_isRequiredValidationArray" -v _lengthValidationArray="$_lengthValidationArray" -v _maxLengthValidationArray="$_maxLengthValidationArray" -v _minLengthValidationArray="$_minLengthValidationArray" -f schemaValidator.awk $1
--- schemaValidator.awk
+++ schemaValidator.awk
@@ -3,6 +3,8 @@
         len=split(columnIndexs,columnIndexsArr," ")
         split(_isRequiredValidationArray,isRequiredValidationArr," ")
         split(_lengthValidationArray,lengthValidationArr," ")
+        split(_minLengthValidationArray,minLengthValidationArr," ")
+        split(_maxLengthValidationArray,maxLengthValidationArr," ")
         for(i=1;i<=len;i++) {
             if(isRequiredValidationArr[i] == "true") {
                 if($columnIndexsArr[i]=="" || $columnIndexsArr[i]=="\n" || $columnIndexsArr[i]=="\r") {
@@ -13,6 +15,16 @@
             if(lengthValidationArr[i] != "null") {
                 if(length($columnIndexsArr[i]) != lengthValidationArr[i]){
                     print "Length validation error in column " columnIndexsArr[i] " and row " NR
+                }
+            }
+            if(maxLengthValidationArr[i] != "null") {
+                if(length($columnIndexsArr[i]) > maxLengthValidationArr[i]){
+                    print "Max length error in column " columnIndexsArr[i] " and row " NR
+                }
+            }
+            if(minLengthValidationArr[i] != "null") {
+                if(length($columnIndexsArr[i]) < minLengthValidationArr[i]){
+                    print "Min length error in column " columnIndexsArr[i] " and row " NR
                 }
             }
         }
