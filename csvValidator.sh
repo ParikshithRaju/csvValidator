eedb5ffb1b43bb6f96812fd55cb63aa4a863f1f4
ParikshithRaju <54943043+ParikshithRaju@users.noreply.github.com>
2023-03-19 22:55:08 +0530

    Feature/pcr/type validations (#2)
    * added type validations
    
    * took delimiter input
    
    ---------
    
    Co-authored-by: Parikshith C R <parikshith.cr@tsgforce.com>

--- csvValidator.sh
+++ csvValidator.sh
@@ -19,6 +19,13 @@ getColumnIndexByString () {
 keysToBeValidated="$(cat $2 | jq ".columnsToBeValidated | keys_unsorted" | tr ',' ' ' | tr ':' ' ')";
 arrayOfKeys=${keysToBeValidated:2:-2};
 isHeaderPresent="$(cat $2 | jq ".isHeaderPresent")"
+delimiterInJson="$(cat $2 | jq ".delimiter")"
+if [[ $delimiterInJson == "null" ]];
+then
+    delimiter=","
+else
+    delimiter=${delimiterInJson:1:-1}
+fi
 declare -a columnIndexesOfColumnsToBeValidated
 declare -a isRequiredValidationArray
 declare -a lengthValidationArray
@@ -30,6 +37,7 @@ do
     requiredLength="$(echo $columnSchema | jq '.length')";
     minLength="$(echo $columnSchema | jq '.minLength')";
     maxLength="$(echo $columnSchema | jq '.maxLength')";
+    typeVal="$(echo $columnSchema | jq '.type')";
     columnsInCSV="$(sed -n '1p' $1)";
     if [[ $isHeaderPresent == "true" ]];
     then
@@ -42,6 +50,7 @@ do
     lengthValidationArray[keyIndex]=$requiredLength
     maxLengthValidationArray[keyIndex]=$maxLength
     minLengthValidationArray[keyIndex]=$minLength
+    typeValidationArray[keyIndex]=${typeVal:1:-1}
     (( ++keyIndex ))
 done
 columnIndexs=${columnIndexesOfColumnsToBeValidated[@]}
@@ -49,5 +58,6 @@ _isRequiredValidationArray=${isRequiredValidationArray[@]}
 _lengthValidationArray=${lengthValidationArray[@]}
 _maxLengthValidationArray=${maxLengthValidationArray[@]}
 _minLengthValidationArray=${minLengthValidationArray[@]}
+_typeValidationArray=${typeValidationArray[@]}
 
-awk -F , -v isHeaderPresent="$isHeaderPresent" -v columnIndexs="$columnIndexs" -v _isRequiredValidationArray="$_isRequiredValidationArray" -v _lengthValidationArray="$_lengthValidationArray" -v _maxLengthValidationArray="$_maxLengthValidationArray" -v _minLengthValidationArray="$_minLengthValidationArray" -f schemaValidator.awk $1
+awk -F $delimiter -v isHeaderPresent="$isHeaderPresent" -v columnIndexs="$columnIndexs" -v _isRequiredValidationArray="$_isRequiredValidationArray" -v _lengthValidationArray="$_lengthValidationArray" -v _maxLengthValidationArray="$_maxLengthValidationArray" -v _minLengthValidationArray="$_minLengthValidationArray" -v _typeValidationArray="$_typeValidationArray" -f schemaValidator.awk $1
