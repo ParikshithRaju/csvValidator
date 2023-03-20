ba019000050f5545aa95aa8691dafde76013a485
Parikshith C R <parikshith.cr@tsgforce.com>
2023-03-21 05:13:12 +0530

    added back delimiter

--- csvValidator.sh
+++ csvValidator.sh
@@ -60,4 +60,4 @@ _maxLengthValidationArray=${maxLengthValidationArray[@]}
 _minLengthValidationArray=${minLengthValidationArray[@]}
 _typeValidationArray=${typeValidationArray[@]}
 
-awk -F , -v isHeaderPresent="$isHeaderPresent" -v columnIndexs="$columnIndexs" -v _isRequiredValidationArray="$_isRequiredValidationArray" -v _lengthValidationArray="$_lengthValidationArray" -v _maxLengthValidationArray="$_maxLengthValidationArray" -v _minLengthValidationArray="$_minLengthValidationArray" -v _typeValidationArray="$_typeValidationArray" -f schemaValidator.awk $1
+awk -F $delimiter -v isHeaderPresent="$isHeaderPresent" -v columnIndexs="$columnIndexs" -v _isRequiredValidationArray="$_isRequiredValidationArray" -v _lengthValidationArray="$_lengthValidationArray" -v _maxLengthValidationArray="$_maxLengthValidationArray" -v _minLengthValidationArray="$_minLengthValidationArray" -v _typeValidationArray="$_typeValidationArray" -f schemaValidator.awk $1
